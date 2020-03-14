73284b0cee98e64b4134392fa6e10923f806344b
sofyan48 <sofyan.saputra@ralali.com>
2020-03-14 18:12:46 +0700

    User Example

--- src/app/v1/api/user/controller/user.go
+++ src/app/v1/api/user/controller/user.go
@@ -1,1 +1,24 @@
 package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/sofyan48/nemo/src/app/utility/rest"
+)
+
+// UserController ...
+type UserController struct{}
+
+// UserControllerHandler ...
+func UserControllerHandler() *UserController {
+	return &UserController{}
+}
+
+// UserControllerInterface ...
+type UserControllerInterface interface{}
+
+// UserCreate ...
+func (ctrl *UserController) UserCreate(contet *gin.Context) {
+	rest.ResponseMessages(context, http.StatusOK, "OK")
+}
